326ad34f3d3dfe2131b3801285c948ad77ab6c67
CristhianCastillo <cece9707@gmail.com>
2020-08-22 00:01:42 -0500

    Inicializacion del Proyecto
    - Se realiza la inicializacion del proyecto.

---
+++ schedule-pilot-core/src/main/java/com/schedulepilot/core/SchedulePilotCoreApplication.java
@@ -0,0 +1,13 @@
+package com.schedulepilot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePilotCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulePilotCoreApplication.class, args);
+	}
+
+}

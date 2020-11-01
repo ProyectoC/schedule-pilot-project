f8e18cdab7627ecd831f604adc79733c2b91cc58
Cristhian Castillo <cece9707@gmail.com>
2020-10-31 20:56:14 -0500

    Se agregan Entidades nuevas para la gestion de prestamos.

---
+++ schedule-pilot-core/src/main/java/com/schedulepilot/core/entities/id/ProductRolId.java
@@ -0,0 +1,12 @@
+package com.schedulepilot.core.entities.id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductRolId {
+
+}

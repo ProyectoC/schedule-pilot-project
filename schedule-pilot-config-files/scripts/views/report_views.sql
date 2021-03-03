03e069fdcab5f7516cffa555facb2fa29bc01b46
Cristhian Castillo <cece9707@gmail.com>
2021-03-03 12:40:20 -0500

    Ajustes de Dependencias y Propiedades

--- schedule-pilot-config-files/scripts/views/report_views.sql
+++ schedule-pilot-config-files/scripts/views/report_views.sql
@@ -39,61 +39,80 @@ GROUP BY c.product_name;
 
 SELECT * FROM rol_account ra;
 
-
 -----
 SELECT
-c.user_account_id,
-c.username,
-count(*)
-FROM v_chart_loan_product c
-WHERE c.date >= '2020-01-01' AND c.date <= '2021-12-31'
--- AND c.user_account_id = 14
-GROUP BY c.user_account_id, c.username
-ORDER BY count(*) DESC;
-
-
-
------
-CREATE VIEW v_chart_requests_vs_loans
-AS
-SELECT
-row_number() OVER () AS id,
+ua.id,
 ua.username,
-a.count AS requests,
-b.count AS loans
-FROM (SELECT
-rci.user_account_id_fk,
-count(*)
-FROM request_check_in rci
-GROUP BY rci.user_account_id_fk) AS a
-INNER JOIN (SELECT
-rci.user_account_id_fk,
-count(*)
-FROM schedule_pilot_db.ticket_check_in tci
-INNER JOIN schedule_pilot_db.request_check_in rci ON tci.request_check_in_id_fk = rci.id
+CAST(a.count AS DECIMAL ) AS requests ,
+b.count ::DECIMAL AS loans
+FROM (
+     	SELECT
+		rci.user_account_id_fk,
+		count(*)
+		FROM schedule_pilot_db.request_check_in rci
+		WHERE rci.created_date >= '2020-01-01' AND rci.created_date <= '2021-12-31'
+		-- AND rci.user_account_id_fk = 14
+		GROUP BY rci.user_account_id_fk
+     ) AS a
+INNER JOIN (
+				SELECT
+				rci.user_account_id_fk,
+				count(*)
+				FROM schedule_pilot_db.ticket_check_in tci
+				INNER JOIN schedule_pilot_db.request_check_in rci ON tci.request_check_in_id_fk = rci.id
+				WHERE tci.created_date >= '2020-01-01' AND tci.created_date <= '2021-12-31'
+				-- AND rci.user_account_id_fk = 14
 GROUP BY rci.user_account_id_fk
-) AS b ON a.user_account_id_fk = b.user_account_id_fk
+           ) AS b ON a.user_account_id_fk = b.user_account_id_fk
 INNER JOIN schedule_pilot_db.user_account ua ON a.user_account_id_fk = ua.id;
+
+
 
 -- a
 SELECT
 rci.user_account_id_fk,
-count(*)
-FROM request_check_in rci
-GROUP BY rci.user_account_id_fk
+rci.created_date
+FROM schedule_pilot_db.request_check_in rci;
 
 -- b
 SELECT
 rci.user_account_id_fk,
-count(*)
+tci.created_date
 FROM schedule_pilot_db.ticket_check_in tci
+INNER JOIN schedule_pilot_db.request_check_in rci ON tci.request_check_in_id_fk = rci.id;
+
+
+
+
+-----
+SELECT
+ua.username,
+pco.created_date AS penalty_date,
+pco.price_penalty,
+i."name" AS item_name,
+tci.created_date,
+tci.return_date
+FROM schedule_pilot_db.penalty_check_out pco
+INNER JOIN schedule_pilot_db.ticket_check_out tco ON pco.ticket_check_out_id_fk = tco.id
+INNER JOIN schedule_pilot_db.ticket_check_in tci ON tco.ticket_check_in_id_fk = tci.id
+INNER JOIN schedule_pilot_db.item i ON tci.item_id_fk = i.id
 INNER JOIN schedule_pilot_db.request_check_in rci ON tci.request_check_in_id_fk = rci.id
-GROUP BY rci.user_account_id_fk;
+INNER JOIN schedule_pilot_db.user_account ua ON rci.user_account_id_fk = ua.id;
 
 
+SELECT
+ua.id,
+ua.username,
+sum(pco.price_penalty)
+FROM schedule_pilot_db.penalty_check_out pco
+INNER JOIN schedule_pilot_db.ticket_check_out tco ON pco.ticket_check_out_id_fk = tco.id
+INNER JOIN schedule_pilot_db.ticket_check_in tci ON tco.ticket_check_in_id_fk = tci.id
+INNER JOIN schedule_pilot_db.request_check_in rci ON tci.request_check_in_id_fk = rci.id
+INNER JOIN schedule_pilot_db.user_account ua ON rci.user_account_id_fk = ua.id
+GROUP BY ua.id, ua.username;
 
 
-
+SELECT * FROM schedule_pilot_db.penalty_check_out;
 
 
 
